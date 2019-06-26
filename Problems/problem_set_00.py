7f23d347279b201eb718c96541ed4681984defc4
pzivich <pzivich@live.unc.edu>
2019-06-26 15:25:34 -0400

    Adding problem set 1

---
+++ Problems/problem_set_00.py
@@ -0,0 +1,111 @@
+###################################################################################################################
+# Problem Set 00: Python Basics
+#
+# Advice: if an error occurs and you aren't sure why, you can always comment out that code and try to fix it later
+#         If you still can't figure it out, you can open an Issue on the GitHub page
+###################################################################################################################
+
+#############
+# Question 1:
+print('Question 1: \nNo output')
+# Indicate whether the following items are mutable or immutable (fill in your answer in the comments)
+
+#   list:
+#   set:
+#   tuple:
+#   dictionary:
+
+#############
+# Question 2:
+print('\nQuestion 2:')
+# Correct the spellings in the following list
+
+q2_var = ['fisrt', 'xecond', 'third', 'fourth', 'fivth', 'sixth', 'sewenth', 'eiight', 'nine', 'tenht']
+# WRITE CODE HERE
+print(q2_var)
+
+#############
+# Question 3:
+print('\nQuestion 3:')
+# Solve the following math problem (remember order of operations):
+#       (Five times six) to the second power plus ((ten minus three) times 2) to the 3/2 power minus one thousand
+
+# WRITE CODE HERE
+
+#############
+# Question 4:
+print('\nQuestion 4:')
+# How many unique items are in the following list? (don't count them by hand!)
+
+q4_var = [1, 2, 1, 'number', 'five', 3, 1, 6, 7, 10, 9, 111, 0.914, 0.913, 0.913, 0.9132, 0.913, 66, 42, 0.914,
+          'delta', 1, 'omega', 7, 12, 21, 'omega', 'five', 99, [1, 1, 3], {5, 6}, 1, 'fifty', 32, 99, 99, 99, 99,
+          99, 99, 96, 99, 99, 99, 99, 99, 99, 'omega']
+# WRITE CODE HERE
+
+#############
+# Question 5:
+print('\nQuestion 5:')
+# Write code to check the type of the following variable
+
+q5_var = range
+# WRITE CODE HERE
+
+#############
+# Question 6:
+print('\nQuestion 6:')
+# A: From the following list print only the second to the third last items
+
+q6_var = [0, 32, 51, 2, 41, 653, 31, 'words', 731, 11, 52, 53, 54, 55, 5, 7, 'last']
+# WRITE CODE HERE
+
+# B: Print only the fourth last items to the last item
+# WRITE CODE HERE
+
+# C: Print only the first to fifth item
+# WRITE CODE HERE
+
+#############
+# Question 7:
+print('\nQuestion 7:')
+# A: Write code to append the second tuple to the end of the first tuple
+
+q7_tuple1 = (2, 4, 6, 8, 10)
+q7_tuple2 = (12, 14, 16, 18, 20, 22, 24)
+# WRITE CODE HERE
+print(q7_tuple1)
+
+# B: Is there an alternative way you could have written that code to do the same thing?
+# WRITE ALTERNATIVE CODE HERE
+
+#############
+# Question 8:
+print('\nQuestion 8:')
+# Remember to index a list inside of a list, you will need to use two sets of brackets, ex. list_to_index[...][...]
+# A: From the following list of lists, print the entire second list (you will only need the first bracket for this Q)
+
+q8_list = [[1, 2, 3, 4, 5],
+           [6, 7, 8, 9, 10],
+           [11, 12, 13, 14, 15],
+           [16, 17, 18, 19, 20],
+           [21, 22, 23, 24, 25]]
+# WRITE CODE HERE
+
+# B: Print the first 3 items from the 4th list
+# WRITE CODE HERE
+
+# C: Print the last item from the last list. What is another way you could have written that code?
+# WRITE CODE HERE
+
+#############
+# Question 9:
+print('\nQuestion 9:')
+# Below is some code I have written. I have indicated three lines that will cause Errors. Your challenge is to fix
+#   my code so that it runs. Delete the first '#' of every line when you are ready to try to fix the code
+
+#print('Hello world! It is me, your computer'  # first problem
+#print('There are problems in this code')
+#print(I need your help fixing this code)  # second problem
+#print('Help me answer the following math problem')
+#x = (5 ++ 7)**2  # third problem
+#print('The answer is', x)
+#print('Thank you for the help!')
